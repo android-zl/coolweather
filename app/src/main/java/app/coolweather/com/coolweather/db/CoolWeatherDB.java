508ec001b7041722d0b00cfd0694aa87695dd350
zl <15933778621@16.com>
2017-08-25 14:45:15 +0800

    CoolWeatherDB 移动到db包下

--- app/src/main/java/app/coolweather/com/coolweather/db/CoolWeatherDB.java
+++ app/src/main/java/app/coolweather/com/coolweather/db/CoolWeatherDB.java
@@ -1,4 +1,4 @@
-package app.coolweather.com.coolweather.util;
+package app.coolweather.com.coolweather.db;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -8,7 +8,6 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
-import app.coolweather.com.coolweather.db.CoolWeatherOpenHelper;
 import app.coolweather.com.coolweather.model.City;
 import app.coolweather.com.coolweather.model.County;
 import app.coolweather.com.coolweather.model.Province;
